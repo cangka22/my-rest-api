d91ccb89cf3a2edaa5f85a819c236413d4b6622e
Renald Abthal Alwafi <34421708+cangka22@users.noreply.github.com>
2023-04-28 00:44:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Naldzzzz'// yourname
+global.MONGO_DB_URI = "mongodb+srv://cangka22:<password>@cluster0.cj5g5gr.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "renaldapang7@gmail.com" //email
+global.email_password = "nrubmdmjfgkzpyhv" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
